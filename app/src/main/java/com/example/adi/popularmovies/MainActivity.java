fbf44cf6cc862ca43eccd7b3ff0f77c8d2bf4117
adi <adi@adi-pc.com>
2018-02-27 14:46:03 +0200

    added check for missing API key

--- app/src/main/java/com/example/adi/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/adi/popularmovies/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
+import com.example.adi.popularmovies.utils.Config;
 import com.example.adi.popularmovies.utils.Network;
 
 
@@ -19,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        if (Config.API_KEY == ""){
+            Toast.makeText(this, R.string.api_key_error, Toast.LENGTH_LONG).show();
+
+            final AppCompatActivity activity = this;
+
+            new AsyncTask<Void, Void, Void>(){
+
+                @Override
+                protected Void doInBackground(Void... voids) {
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    moveTaskToBack(true);
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                }
+            }.execute();
+        }
 
         final Context context = this;
 
